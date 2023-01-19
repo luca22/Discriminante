8bff2254a62854f128c0b1c85ef3c2dab2736fe5
Luca Lanzetta <78610431+luca22@users.noreply.github.com>
2023-01-19 18:05:24 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+int main(void) {
+
+	double dis;
+
+	dis=discriminante(1.0,2.0,3.0);
+	dis=discriminante(0.0,2.0,3.0);
+	dis=discriminante(1.0,0.0,3.0);
+	dis=discriminante(1.0,2.0,0.0);
+
+	return 0;
+}
